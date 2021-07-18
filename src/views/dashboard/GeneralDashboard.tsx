8734367a7dca61c1da35d2cfbef5ef574f80d37e
Cole Killian <g.cole.killian@gmail.com>
2021-07-18 10:19:12 -0700

    Add some more dashboard welcome cards

--- src/views/dashboard/GeneralDashboard.tsx
+++ src/views/dashboard/GeneralDashboard.tsx
@@ -13,9 +13,9 @@ const GeneralDashboard = () => {
   return (
     <div>
       <Header title="Dashboard" crumbs={["Dashboard"]} />
-      <Grid container>
+      <Grid container spacing={4}>
         <Grid item xs={8}>
-          <Card style={{ backgroundColor: "#C8FACD" }}>
+          <Card style={{ backgroundColor: "#C8FACD", height: "100%" }}>
             <CardContent style={{ display: "flex", padding: "40px" }}>
               <div style={{ width: "50%", margin: "30px 0" }}>
                 <Typography gutterBottom variant="h5" component="h2">
@@ -44,6 +44,73 @@ const GeneralDashboard = () => {
               />
             </CardContent>
           </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card style={{ backgroundColor: "#C8FACD" }}>
+            <CardContent style={{ display: "flex", padding: "40px" }}>
+              <div style={{ margin: "30px 0" }}>
+                <Typography gutterBottom variant="h5" component="h2">
+                  <Box fontWeight="bold">Set up your billing account</Box>
+                </Typography>
+                <Typography gutterBottom variant="body1" component="h3">
+                  Take a few seconds to set up billing so that you can start
+                  distributing perks to your employees :)
+                </Typography>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={{
+                    marginTop: "30px",
+                    fontWeight: "bold",
+                    backgroundColor: "#00AB55",
+                  }}
+                >
+                  Set up billing
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={8}>
+          <Card style={{ backgroundColor: "#C8FACD" }}>
+            <CardContent style={{ display: "flex", padding: "40px" }}>
+              <div style={{ width: "50%", margin: "30px 0" }}>
+                <Typography gutterBottom variant="h5" component="h2">
+                  <Box fontWeight="bold">Set up your billing account</Box>
+                </Typography>
+                <Typography gutterBottom variant="body1" component="h3">
+                  Take a few seconds to set up billing so that you can start
+                  distributing perks to your employees :)
+                </Typography>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={{
+                    marginTop: "30px",
+                    fontWeight: "bold",
+                    backgroundColor: "#00AB55",
+                  }}
+                >
+                  Set up billing
+                </Button>
+              </div>
+              <img
+                src="/welcome_graphic.svg"
+                alt="Welcome Graphic"
+                style={{ width: "300px", marginLeft: "auto" }}
+              />
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card
+            style={{
+              backgroundColor: "#C8FACD",
+              height: "100%",
+              width: "100%",
+            }}
+          ></Card>
         </Grid>
       </Grid>
     </div>
