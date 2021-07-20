3788db2337accd03a52e64cae41995ab5f919ada
Cole Killian <g.cole.killian@gmail.com>
2021-07-20 16:19:28 -0700

    update dashboard and add undraw files

--- src/views/dashboard/MetricCard.tsx
+++ src/views/dashboard/MetricCard.tsx
@@ -1,32 +1,33 @@
-import Header from "components/Header";
-import React from "react";
+import { Box, Card, Typography } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
-import {
-  Button,
-  Card,
-  MenuItem,
-  Select,
-  TextField,
-  Typography,
-} from "@material-ui/core";
-import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
-import PaymentIcon from '@material-ui/icons/Payment';
+import React from "react";
 
-const MetricCard = ({title, number, children}) => {
+const MetricCard = ({ title, number, icon }) => {
   return (
     <div>
-
-        <Card style={{ width: "90%", padding: 10, height: 130 }} elevation={4}> 
-          <Grid container spacing={0} direction="row" alignItems="center" justifyContent="center">
-            <Grid item xs={6}> <b style={{fontSize: 14, marginLeft: 3}}>{title}</b> 
-            <h1>{number}</h1>
-            </Grid>
-            <Grid item xs={6}> {children}</Grid>
+      <Card style={{ padding: 30 }} elevation={4}>
+        <Grid
+          container
+          spacing={0}
+          direction="row"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Grid item xs={6}>
+            <b style={{ fontSize: 14, marginBottom: "20px", display: "block" }}>
+              {title}
+            </b>
+            <Typography variant="h4">
+              <Box fontWeight={800}>{number}</Box>
+            </Typography>
           </Grid>
-        
-        </Card> 
+          <Grid item xs={6}>
+            {icon}
+          </Grid>
+        </Grid>
+      </Card>
     </div>
   );
 };
 
-export default MetricCard; 
+export default MetricCard;
