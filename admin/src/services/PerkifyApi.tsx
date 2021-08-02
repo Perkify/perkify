42db960276a4b6026ac764ca5a22cf301ac1604e
Cole Killian <g.cole.killian@gmail.com>
2021-08-02 17:44:49 -0400

    Firebase environments (#100)
    * add production firebase config
    
    * making progress, but not high enough priority for now
    
    * process.env.environment switches
    
    * update stripe secret keys
    
    * update allPerks with production and dev info
    
    * export allPerks properly
    
    * configure firebaserc
    
    * don't predeploy build
    
    don't predeploy build because it makes it harder to control what is deployed in CI
    
    * add more npm scripts
    
    add scripts for differentiating between staging and production builds.
    
    update the github action to use the staging build
    
    use staging build by default
    
    * fix allPerks
    
    netflix.jpg -> netflix.jpeg
    
    add Img to allPerksDict
    
    * add more npm scripts
    
    add npm scripts for starting apps with different firebase environments

--- admin/src/services/PerkifyApi.tsx
+++ admin/src/services/PerkifyApi.tsx
@@ -2,9 +2,11 @@ import axios from 'axios';
 
 const PerkifyApi = axios.create({
   baseURL:
-    process.env.NODE_ENV == 'development'
-      ? 'http://localhost:5001/perkify-5790b/us-central1'
-      : 'https://us-central1-perkify-5790b.cloudfunctions.net',
+    process.env.REACT_APP_FIREBASE_ENVIRONMENT == 'production'
+      ? 'https://us-central1-perkify-prod.cloudfunctions.net'
+      : process.env.REACT_APP_FIREBASE_ENVIRONMENT == 'staging'
+      ? 'https://us-central1-perkify-5790b.cloudfunctions.net'
+      : 'http://localhost:5001/perkify-5790b/us-central1',
 });
 
 PerkifyApi.defaults.withCredentials = true;
--- user/src/firebaseApp.ts
+++ user/src/firebaseApp.ts
@@ -1,8 +1,19 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/functions';
 
-const firebaseConfig = {
+const prodFirebaseConfig = {
+  apiKey: 'AIzaSyA8MFTaagEWsF3-9tUL1GvZq3OGeA4XL5k',
+  authDomain: 'perkify-prod.firebaseapp.com',
+  projectId: 'perkify-prod',
+  storageBucket: 'perkify-prod.appspot.com',
+  messagingSenderId: '729019735857',
+  appId: '1:729019735857:web:389e4ad51ab9d51fd45a10',
+  measurementId: 'G-XG60T5JEF1',
+};
+
+const devFirebaseConfig = {
   apiKey: 'AIzaSyDy0YjntwrgEwpDVGbqHtJfUtl3fu-9so4',
   authDomain: 'perkify-5790b.firebaseapp.com',
   projectId: 'perkify-5790b',
@@ -12,10 +23,15 @@ const firebaseConfig = {
   measurementId: 'G-CJNSWW5F87',
 };
 
-const app = firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(
+  process.env.REACT_APP_FIREBASE_ENVIRONMENT == 'production'
+    ? prodFirebaseConfig
+    : devFirebaseConfig
+);
 
 const db = app.firestore();
 const auth = app.auth();
+const functions = app.functions();
 
 if (process.env.REACT_APP_FIRESTORE_MODE == 'emulator') {
   console.info('Using emulator firestore');
@@ -23,4 +39,4 @@ if (process.env.REACT_APP_FIRESTORE_MODE == 'emulator') {
 }
 
 export default app;
-export { db, auth };
+export { db, auth, functions };
