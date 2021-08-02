90ddc340b6615075c4682398bfce8bbfdd46ff53
Cole Killian <g.cole.killian@gmail.com>
2021-08-01 19:55:05 -0700

    update allPerks with production and dev info

--- functions/shared/allPerks.ts
+++ functions/shared/allPerks.ts
@@ -1,4 +1,67 @@
-export const allPerks = [
+export const allProductionPerks = [
+  {
+    Name: 'Netflix Standard',
+    Cost: 13.99,
+    Period: 'Monthly',
+    stripePriceId: 'price_1JJqw9KuQQHSHZsm71r4kkhd',
+    Img: 'netflix.jpg',
+    Product: 'prod_JxmVIY3P8jV19L',
+    NetworkId: '123',
+    PaymentName: 'Netflix Standard',
+  },
+  {
+    Name: 'Hulu',
+    Cost: 5.99,
+    Period: 'Monthly',
+    stripePriceId: 'price_1JJqwcKuQQHSHZsmQ2JFPQwp',
+    Img: 'hulu.png',
+    Product: 'prod_JxmV7NAPQLFyOl',
+    NetworkId: '123',
+    PaymentName: 'Hulu',
+  },
+  {
+    Name: 'Audible Plus',
+    Cost: 7.95,
+    Period: 'Monthly',
+    stripePriceId: 'price_1JJqwxKuQQHSHZsmosyOePOl',
+    Img: 'audible.png',
+    Product: 'prod_JxmVInB54WZwQp',
+    NetworkId: '123',
+    PaymentName: 'Audible Plus',
+  },
+  {
+    Name: 'Disney Plus',
+    Cost: 7.99,
+    Period: 'Monthly',
+    stripePriceId: 'price_1JJqpKKuQQHSHZsmZugC5Zxi',
+    Img: 'disneyplus.png',
+    Product: 'prod_JxmO9TVTtcHLd9',
+    NetworkId: '123',
+    PaymentName: 'Disney Plus',
+  },
+  {
+    Name: 'Headspace',
+    Cost: 12.99,
+    Period: 'Monthly',
+    stripePriceId: 'price_1JJqrcKuQQHSHZsmPSI9mrP0',
+    Img: 'headspace.png',
+    Product: 'prod_JxmQHmjecXibCK',
+    NetworkId: '123',
+    PaymentName: 'Headspace',
+  },
+  {
+    Name: 'Spotify Individual',
+    Cost: 9.99,
+    Period: 'Monthly',
+    stripePriceId: 'price_1JJqs2KuQQHSHZsmfzgIOrJ3',
+    Img: 'spotify.jpg',
+    Product: 'prod_JxmQVILWLae2ZZ',
+    NetworkId: '123',
+    PaymentName: 'Spotify Individual',
+  },
+];
+
+export const allDevelopmentPerks = [
   {
     Name: 'Netflix Standard',
     Cost: 13.99,
@@ -60,6 +123,11 @@ export const allPerks = [
     PaymentName: 'Spotify Individual',
   },
 ];
+
+const allPerks =
+  process.env.FIREBASE_STRIPE_ENVIRONMENT == 'production'
+    ? allProductionPerks
+    : allDevelopmentPerks;
 
 export const allPerksDict = allPerks.reduce(
   (map, perk) => (
--- user/src/shared/index.ts
+++ user/src/shared/index.ts
@@ -1,11 +1,73 @@
-// export * from '../../../functions/shared';
-export const allPerks = [
+export const allProductionPerks = [
+  {
+    Name: 'Netflix Standard',
+    Cost: 13.99,
+    Period: 'Monthly',
+    stripePriceId: 'price_1JJqw9KuQQHSHZsm71r4kkhd',
+    Img: 'netflix.jpg',
+    Product: 'prod_JxmVIY3P8jV19L',
+    NetworkId: '123',
+    PaymentName: 'Netflix Standard',
+  },
+  {
+    Name: 'Hulu',
+    Cost: 5.99,
+    Period: 'Monthly',
+    stripePriceId: 'price_1JJqwcKuQQHSHZsmQ2JFPQwp',
+    Img: 'hulu.png',
+    Product: 'prod_JxmV7NAPQLFyOl',
+    NetworkId: '123',
+    PaymentName: 'Hulu',
+  },
+  {
+    Name: 'Audible Plus',
+    Cost: 7.95,
+    Period: 'Monthly',
+    stripePriceId: 'price_1JJqwxKuQQHSHZsmosyOePOl',
+    Img: 'audible.png',
+    Product: 'prod_JxmVInB54WZwQp',
+    NetworkId: '123',
+    PaymentName: 'Audible Plus',
+  },
+  {
+    Name: 'Disney Plus',
+    Cost: 7.99,
+    Period: 'Monthly',
+    stripePriceId: 'price_1JJqpKKuQQHSHZsmZugC5Zxi',
+    Img: 'disneyplus.png',
+    Product: 'prod_JxmO9TVTtcHLd9',
+    NetworkId: '123',
+    PaymentName: 'Disney Plus',
+  },
+  {
+    Name: 'Headspace',
+    Cost: 12.99,
+    Period: 'Monthly',
+    stripePriceId: 'price_1JJqrcKuQQHSHZsmPSI9mrP0',
+    Img: 'headspace.png',
+    Product: 'prod_JxmQHmjecXibCK',
+    NetworkId: '123',
+    PaymentName: 'Headspace',
+  },
+  {
+    Name: 'Spotify Individual',
+    Cost: 9.99,
+    Period: 'Monthly',
+    stripePriceId: 'price_1JJqs2KuQQHSHZsmfzgIOrJ3',
+    Img: 'spotify.jpg',
+    Product: 'prod_JxmQVILWLae2ZZ',
+    NetworkId: '123',
+    PaymentName: 'Spotify Individual',
+  },
+];
+
+export const allDevelopmentPerks = [
   {
     Name: 'Netflix Standard',
     Cost: 13.99,
     Period: 'Monthly',
     stripePriceId: 'price_1JInq1KuQQHSHZsmq9prpaUr',
-    Img: 'netflix.jpeg',
+    Img: 'netflix.jpg',
     Product: 'prod_JwhE2ADq37oUrZ',
     NetworkId: '123',
     PaymentName: 'Netflix Standard',
@@ -62,6 +124,11 @@ export const allPerks = [
   },
 ];
 
+const allPerks =
+  process.env.FIREBASE_STRIPE_ENVIRONMENT == 'production'
+    ? allProductionPerks
+    : allDevelopmentPerks;
+
 export const allPerksDict = allPerks.reduce(
   (map, perk) => (
     (map[perk.Name] = {
@@ -69,7 +136,6 @@ export const allPerksDict = allPerks.reduce(
       Period: perk['Period'],
       Name: perk.Name,
       stripePriceId: perk.stripePriceId,
-      Img: perk.Img,
       PaymentName: perk.PaymentName,
       Product: perk.Product,
       NetworkId: perk.NetworkId,
