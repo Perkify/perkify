38a30284d2c030c9375ab17c7a2b8b13e820d2ae
Cole Killian <g.cole.killian@gmail.com>
2021-08-22 19:14:15 -0400

    remove stripe listener because less webhooks

--- functions/startStripeListeners.sh
+++ functions/startStripeListeners.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 st -e sh -c "stripe listen --forward-to http://localhost:5001/perkify-5790b/us-central1/syncToFirestoreWebhookHandler" &
-st -e sh -c "stripe listen --forward-to http://localhost:5001/perkify-5790b/us-central1/invoicePaidWebhookHandler" &
+# st -e sh -c "stripe listen --forward-to http://localhost:5001/perkify-5790b/us-central1/invoicePaidWebhookHandler" &
 # st -e sh -c "stripe listen --forward-to http://localhost:5001/perkify-5790b/us-central1/issuingAuthorizationRequestWebhookHandler" &
 
 wait
